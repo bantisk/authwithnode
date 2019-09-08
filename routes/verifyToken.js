884b6b9d7cf9a2a6f0ebd2a7141a26ea2e5a3e12
bantisk <dinesh.s.kulkarni@gmail.com>
2019-09-08 11:41:40 -0400

    change

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-module.export = function (req, res, next) {
+module.exports = function (req, res, next) {
     const token = req.header('auth-token');
     if (!token) {
         res.status(400).send('Access Denied, Login to post something');
